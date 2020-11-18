f6a779cd0a6738be1d5d5f474bf2e0bbb93d148f
xiongxianzhu <929679459@qq.com>
2020-11-18 10:31:16 +0800

    以类形式生成图片验证码, 返回验证码和图片验证码图片的base64字符串

--- src/image.py
+++ src/image.py
@@ -8,6 +8,8 @@ from captcha.image import ImageCaptcha
 
 __all__ = [
     'create_captcha',
+    'gen_verify_code',
+    'Captcha',
 ]
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -28,9 +30,37 @@ def create_captcha():
     image.write(code, '{code}.png'.format(code=code))
 
 
-def gen_verify_code(length):
+def gen_verify_code(length=4):
     """ 生成随机验证码 """
     return ''.join(random.choice(ALLOWED_CHARS) for _ in range(length))
 
+
+class Captcha(object):
+    """ 类形式生成图片验证码 """
+
+    def __init__(self, code_len=4, fonts=DEFAULT_FONTS, allowed_chars=ALLOWED_CHARS):
+        self.code_len = code_len
+        self.fonts = fonts
+        self.allowed_chars = allowed_chars
+
+    def create_captcha(self):
+        """ 创建图片验证码 """
+        image = ImageCaptcha(fonts=self.fonts)
+        code = gen_verify_code(self.code_len)
+        stream = image.generate(code)
+        # 图片的base64字符串格式：data:image/png;base64,<base64字符串>
+        img_base64 = 'data:image/png;base64,{base64_str}'.format(
+            base64_str=str(base64.b64encode(stream.getvalue()), encoding='utf-8'))
+        return code, img_base64
+
+
+    def gen_verify_code(self):
+        """ 生成随机验证码 """
+        return ''.join(random.choice(self.allowed_chars) for _ in range(self.code_len))
+        
+
 if __name__ == '__main__':
-    create_captcha()
+    # create_captcha()
+    captcha_obj = Captcha()
+    code, img_base64 = captcha_obj.create_captcha()
+    print(code, img_base64)
